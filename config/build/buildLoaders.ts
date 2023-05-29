a2346d1756cf22dd670b08a9c74cfad1db33dce5
Dmitriy Palii <it.n00b.pel@gmail.com>
2023-05-29 16:32:27 +0300

    add babel, add extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -10,6 +10,25 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule [] {
         use: ['@svgr/webpack'],
     };
 
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    ['i18next-extract',
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true,
+                        },
+                    ],
+                ],
+            },
+        },
+    };
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -46,6 +65,7 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule [] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
     ];
