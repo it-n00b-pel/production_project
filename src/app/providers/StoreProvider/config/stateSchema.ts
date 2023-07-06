9390141be504fc6a09d96a6aac81405776fabe06
Dmitriy Palii <it.n00b.pel@gmail.com>
2023-07-06 09:53:23 +0300

    add instance axios

--- src/app/providers/StoreProvider/config/stateSchema.ts
+++ src/app/providers/StoreProvider/config/stateSchema.ts
@@ -8,11 +8,16 @@ import {
     Reducer,
     ReducersMapObject,
 } from '@reduxjs/toolkit';
+import { ProfileSchema } from 'entities/Profile';
+import { AxiosInstance } from 'axios';
+import { NavigateOptions, To } from 'react-router';
 
 export interface StateSchema {
     counter: CounterSchema;
     user: UserSchema;
     loginForm?: LoginSchema;
+    profile: ProfileSchema
+
 }
 
 export type StateSchemaKey = keyof StateSchema;
@@ -26,4 +31,14 @@ export interface ReducerManager {
 
 export interface ReduxStoreWithManager extends EnhancedStore<StateSchema>{
     reducerManager: ReducerManager
+}
+
+export interface ThankExtraArg {
+    api: AxiosInstance,
+    navigate: (to: To, options?: NavigateOptions) => void,
+}
+
+export interface ThunkConfig<T> {
+    rejectValue: T,
+    extra: ThankExtraArg
 }
