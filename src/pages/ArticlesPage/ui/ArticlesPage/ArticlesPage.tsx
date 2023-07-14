4cad539cc5ac03e47f02e40c2a9b772484fb2f18
Dmitriy Palii <it.n00b.pel@gmail.com>
2023-07-14 17:33:33 +0300

    add articles page

---
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -0,0 +1,19 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import cls from './ArticlesPage.module.scss';
+
+interface ArticlesPageProps {
+    className?: string;
+}
+
+const ArticlesPage = ({ className }: ArticlesPageProps) => {
+    const { t } = useTranslation('article');
+
+    return (
+        <div className={classNames(cls.ArticlesPage, {}, [className])}>
+            ARTCICLES PAGE
+        </div>
+    );
+};
+
+export default ArticlesPage;
