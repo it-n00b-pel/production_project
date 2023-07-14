559cf64969b5a444905e3babd6be78ae09c6de81
Dmitriy Palii <it.n00b.pel@gmail.com>
2023-07-14 12:35:35 +0300

    update app router

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -6,6 +6,8 @@ export {
     userReducer,
     userActions,
 } from './model/slice/userSlice';
+
+export { getUserInited } from './model/selectors/getUserInited/getUserInited';
 export {
     UserSchema,
     User,
